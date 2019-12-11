d70a65c26d5216655e0af5800c3dc19fc38659c0
Ítalo Gregório <italo-07@hotmail.com>
2019-12-10 22:01:17 -0300

    Criado entidades de categoria e despesa

---
+++ financas_aspnet_backend/Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace financas_aspnet_backend.Models
+{
+    [Table("Categorias")]
+    public class Categoria
+    {
+        [Key]
+        [Display(Name ="Código")]
+        public int Id { get; set; }
+
+        [Display(Name ="Descrição")]
+        [Required(ErrorMessage ="Campo 'Descrição' é obrigatório!")]
+        public string Descricao { get; set; }
+
+        public ICollection<Despesa> Despesas { get; set; }
+    }
+}
---
+++ financas_aspnet_backend/Models/Despesa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace financas_aspnet_backend.Models
+{
+    [Table("Despesas")]
+    public class Despesa
+    {
+        [Key]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Campo 'Descrição' é obrigatório!")]
+        public string Descricao { get; set; }
+
+        [Display(Name = "Data")]
+        [Required(ErrorMessage = "Campo 'Data' é obrigatório!")]
+        public DateTime Data { get; set; }
+
+        [Display(Name = "Valor")]
+        [Required(ErrorMessage = "Campo 'Valor' é obrigatório!")]
+        public double Valor { get; set; }
+
+        [Display(Name = "Categoria")]
+        [Required(ErrorMessage = "Campo 'Categoria' é obrigatório!")]
+        public int CategoriaId { get; set; }
+
+        [ForeignKey("CategoriaId")]
+        public Categoria Categoria { get; set; }
+    }
+}
